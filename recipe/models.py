92a95d95c69546f7e29edbd3f9371b5e817ac4ca
ほらみたま <horahora0828@gmail.com>
2020-05-27 17:20:51 +0900

    レシピテーブル作成

--- recipe/models.py
+++ recipe/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class RecipeModel(models.Model):
+    name = models.CharField(max_length=20)
+    ingredients = models.TextField()
+    process = models.TextField()
+    category = models.CharField(max_length=20)
