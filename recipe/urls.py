3dd8882985639364413b07e232287f0d1ca3fef8
ほらみたま <horahora0828@gmail.com>
2020-05-29 14:12:35 +0900

    新規投稿機能

--- recipe/urls.py
+++ recipe/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import RecipeList
+from .views import RecipeList, RecipeDetail, RecipeCreate
 
 urlpatterns = [
-    path('list/', RecipeList.as_view())
+    path('list/', RecipeList.as_view(), name='list'),
+    path('detail/<int:pk>', RecipeDetail.as_view(), name='detail'),
+    path('create/', RecipeCreate.as_view(), name='create'),
 ]
--- recipe/views.py
+++ recipe/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import RecipeModel
+from django.urls import reverse_lazy
 
 # Create your views here.
 
 class RecipeList(ListView):
     template_name = 'list.html'
     model = RecipeModel
+
+class RecipeDetail(DetailView):
+    template_name = 'detail.html'
+    model = RecipeModel
+
+class RecipeCreate(CreateView):
+    template_name = 'create.html'
+    model = RecipeModel
+    fields = ('name','ingredients','process','category')
+    success_url = reverse_lazy('list')
